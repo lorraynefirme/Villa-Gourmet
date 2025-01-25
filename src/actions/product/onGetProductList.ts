870a9c709d2c3c2df3d330afae1ce83c5a25b71f
Lorrayne Firme <lorraynefirme@gmail.com>
2025-01-25 16:26:55 -0300

    feat: action para buscar os produtos

---
+++ src/actions/product/onGetProductList.ts
@@ -0,0 +1,21 @@
+"use client";
+
+import { ProductService } from "@/service/http/productService";
+import { ResponseGetProductList } from "@/service/http/productService.interface";
+
+export const onGetProductList = async (): Promise<
+  ResponseGetProductList | undefined
+> => {
+  try {
+    const response = await ProductService.getProductList();
+
+    if (response) {
+      return {
+        data: response.data,
+        message: response.message,
+      };
+    }
+  } catch (error) {
+    console.log("erro na action");
+  }
+};
