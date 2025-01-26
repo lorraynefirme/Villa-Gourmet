ffdb3e175cff3a028b78df63fb09b29b6d51b402
Lorrayne Firme <lorraynefirme@gmail.com>
2025-01-26 09:54:27 -0300

    feat: altera base url

--- src/service/apiService.ts
+++ src/service/apiService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiService = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://api-produtos-one.vercel.app/",
   // headers: { Authorization: "Bearer token" },
 });
 
