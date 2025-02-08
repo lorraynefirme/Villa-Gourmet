595255a2814444f28392d8e8a583611ea0d165c0
Lorrayne Firme <lorraynefirme@gmail.com>
2025-02-08 17:51:38 -0300

    feat: componente do header

---
+++ src/components/header/header.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link"
+
+export const Header = () => {
+
+    return   <div className="flex justify-between bg-amber-500 px-9 py-3 mb-3">
+    <header>
+      <p>Villa Gourmet</p>
+    </header>
+    <nav>
+      <ul className="flex gap-3">
+        <li>
+          <Link className="cursor-pointer" href="/">
+            Pratos
+          </Link>
+        </li>
+        <li>
+          <Link className="cursor-default" href="#">
+            Sobre
+          </Link>
+        </li>
+        <li>
+          <Link className="cursor-default" href="#">
+            Contato
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  </div>
+}
