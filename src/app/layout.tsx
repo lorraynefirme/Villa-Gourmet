a8f24226bd268e9b148cbf6050d51f5e01a91545
Lorrayne Firme <lorraynefirme@gmail.com>
2025-02-15 20:24:23 -0300

    feat: iniciando o msw e adicionando o Toaster no layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,10 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { CartContainer } from "@/domains/cart/views/cartContainer";
-import { Footer } from "@/components/footer/footer";
 import { Header } from "@/components/header/header";
+import { Footer } from "@/components/footer/footer";
+import { initMocks } from "@/lib/msw/setup";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,6 +22,10 @@ export const metadata: Metadata = {
   description: "Restaurante Villa Gourmet",
 };
 
+if (process.env.NODE_ENV === "development") {
+  initMocks();
+}
+
 export default function RootLayout({
   children,
   modal,
@@ -32,12 +38,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex-col bg-blue-50`}
       >
+        <Toaster />
         <Header />
         <div className="flex w-full pb-9 sm:pb-0">
-          <main className="px-3 pb-14 w-full">{children}</main>
-          <aside className="w-80 fixed z-20 right-0">
-            <CartContainer />
-          </aside>
+            <main className="px-3 pb-14 w-full">{children}</main>
+            <aside className="w-80 fixed z-20 right-0">
+              <CartContainer />
+            </aside>
         </div>
         <Footer />
         {modal}
