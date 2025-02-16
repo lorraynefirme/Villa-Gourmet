187777513be4f34d965aa939256b050d0fb0cd28
Lorrayne Firme <lorraynefirme@gmail.com>
2025-02-16 13:08:33 -0300

    remove: remove apiService com instancia do axios

--- src/services/apiService.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const apiService = axios.create({
-  baseURL: "https://api-produtos-one.vercel.app/",
-  // baseURL: "http://localhost:3001/",
-  // headers: { Authorization: "Bearer token" },
-});
-
-export default apiService;
